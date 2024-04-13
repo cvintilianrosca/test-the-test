0644ab29c5c05e8f7541c5fa472885136e745d1d
crosca <quintilianrosca2000@gmail.com>
2024-04-13 13:20:11 +0300

    TASK 4: Alex implemented methods, is happy and goes to home

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,8 @@
 
 // TASK 1 : 3 field-uri in clasa student
 // TASK 2 : add another 3 field-uri in clasa student and another class and rocket science
+// TASK 3:  add some classes
+// TASK 4: add some code (sett add some ifs)
 
 public class Student {
 
@@ -18,4 +20,59 @@ public class Student {
 
     int yearOfSomething;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(final int age) {
+        this.age = age;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(final String university) {
+        this.university = university;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(final String street) {
+        this.street = street;
+    }
+
+    public int getYearOfSomething() {
+        return yearOfSomething;
+    }
+
+    public void setYearOfSomething(final int yearOfSomething) {
+        this.yearOfSomething = yearOfSomething;
+    }
 }
