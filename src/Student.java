c155438945af7995db30da9638ad01466948ad80
crosca <quintilianrosca2000@gmail.com>
2024-04-13 13:15:52 +0300

    TASK 2: Alex implemented something new

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,6 @@
 
 // TASK 1 : 3 field-uri in clasa student
+// TASK 2 : add another 3 field-uri in clasa student and another class and rocket science
 
 public class Student {
 
@@ -8,4 +9,13 @@ public class Student {
     String lastName;
 
     int age;
+
+    String university;
+
+    String city;
+
+    String street;
+
+    int yearOfSomething;
+
 }
